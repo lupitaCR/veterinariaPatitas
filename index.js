13b595d8905a2ad6f4cdafe6c631319b8d17c0ea
lupitaCR <2036000757@utna.edu.mx>
2023-07-24 20:00:00 -0500

    cambio de servidor

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ const express = require('express');// instanciar a express
 const conexion = require("./conexion/conexion.js");//al importar variable usamos llaves
 const app = express();// se ejecutara express en una varible
 const routerUser = require("./routes/routeUsuarios.js");
-
+const routerCitas = require("./routes/routeCitas.js");
+const routerAccesorios = require("./routes/routeAccesorios.js");
 conexion();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.use("/api/user", routerUser);
-//app.use("/api/citas", routerP);
-//app.use("/api/accesorios", routerP);
+app.use("/api/citas", routerCitas);
+app.use("/api/accesorios", routerAccesorios);
 
-app.listen(3000,() => {
- console.log('el servidor esta corriendo en el puerto 3000');
+app.listen(3001,() => {
+ console.log('el servidor esta corriendo en el puerto 3001');
 })
 
